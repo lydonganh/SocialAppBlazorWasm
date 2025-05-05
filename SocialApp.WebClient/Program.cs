58573b385ef7a1a8f237b307c18b0234e615248f
nhu quynh <endoramonica44444@gmail.com>
2025-05-05 10:42:05 +0700

    .WebClient: refactor

---
+++ SocialApp.WebClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using SocialApp.WebClient;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://m3qcc6b6-7175.asse.devtunnels.ms/api") });
+
+await builder.Build().RunAsync();
